d6c69c2fd4665af7f1980a88b3ef17cfe3871200
TANNA TECHBIZ LLP <30870024+tannatechbiz@users.noreply.github.com>
2020-09-30 18:40:27 +0530

    Update tfmini.py

--- tfmini.py
+++ tfmini.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*
 import serial
+
 ser = serial.Serial("/dev/ttyS0", 115200)
+
 def getTFminiData():
-while True:
-count = ser.in_waiting
-if count > 8:
-recv = ser.read(9)
-ser.reset_input_buffer()
-if recv[0] == 'Y' and recv[1] == 'Y': # 0x59 is 'Y'
-low = int(recv[2].encode('hex'), 16)
-high = int(recv[3].encode('hex'), 16)
-distance = low + high * 256
-print(distance)
+    while True:
+        count = ser.in_waiting
+        if count > 8:
+            recv = ser.read(9)
+            ser.reset_input_buffer()
+            if recv[0] == 'Y' and recv[1] == 'Y': # 0x59 is 'Y'
+                low = int(recv[2].encode('hex'), 16)
+                high = int(recv[3].encode('hex'), 16)
+                distance = low + high * 256
+                print(distance)
+                
+
+
 if __name__ == '__main__':
-try:
-if ser.is_open == False:
-ser.open()
-getTFminiData()
-except KeyboardInterrupt: # Ctrl+C
-if ser != None:
-ser.close()
+    try:
+        if ser.is_open == False:
+            ser.open()
+        getTFminiData()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
